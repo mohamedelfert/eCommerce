d62c8f99cb019d0d5ac6788f1e3f0da92aae8d79
mohamedelfert <me_me4152@yahoo.com>
2022-01-17 20:16:59 +0200

    cut and prepare design create layouts and index - home views create admin.php for admin routes and in RoutServiceProvider create mapWebAdminRoutes and call it in map function in admin.php add route to test project

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapWebAdminRoutes();
+
         //
     }
 
@@ -61,6 +63,13 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapWebAdminRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
     }
 
     /**
