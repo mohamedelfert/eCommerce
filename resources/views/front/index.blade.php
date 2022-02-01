58f0dc4f91e87c38fc7cb5cd22da02400e0bfaf7
mohamedelfert <me_me4152@yahoo.com>
2022-02-02 00:52:24 +0200

    in web.php return view('front/home') to show front page. start cutting the front design for website create front folder and create int it ( index.blade - home.blade ) and create layouts folder and inside this folder ( header - navbar - menu - footer - messages - product_widget - promo - slider ) views and include this files in index view and home view .  add ( css - js - img - fonts ) folders and style.css in public/design/front path.

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,8 @@
+@include('front.layouts.header')
+@include('front.layouts.navbar')
+@include('front.layouts.menu')
+@include('front.layouts.messages')
+
+    @yield('content')
+
+@include('front.layouts.footer')
